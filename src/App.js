721611d5870e4856a440eec3904071b5f21540f9
Tulasidas Nayak <tulasidasnayak68@gmail.com>
2024-02-06 19:23:54 +0530

    XDisplayName

---
+++ src/App.js
@@ -0,0 +1,63 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React, { useState } from "react";
+
+function App() {
+
+  const [firstName, setfirstName] = useState('');
+  const [lastName, setlastName] = useState('');
+  const [show, setshow] = useState(false);
+
+  // Function for set first name
+  const handleFirst = (e)=>{
+    setfirstName(e.target.value);
+  };
+  // Function for set last name
+  const handleLast = (e)=>{
+    setlastName(e.target.value);
+  };
+  // Function for submit process
+  const submit = (e)=>{
+    e.preventDefault();
+    if(firstName && lastName){
+      setshow(true);
+    }
+  };
+
+  return (
+    <>
+      <h1>Full Name Display</h1>
+      <form>
+        <label htmlFor="firstName">
+          First Name:
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            onChange={handleFirst}
+            required
+          />
+        </label>
+        <br/>
+        <br/>
+        <label htmlFor="lastName">
+          Last Name:
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            onChange={handleLast}
+            required
+          />
+        </label>
+        <br/>
+        <br/>
+        <button onClick={submit}>Submit</button>
+      </form>
+      <br/>
+      {show ? <p>Full Name: {firstName} {lastName}</p> : <></>}
+    </>
+  );
+}
+
+export default App;
